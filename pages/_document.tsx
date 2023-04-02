5c0383809114acb96877bb97c7af3fc4f081e037
Ariel Schiavoni <arielschiavoni@gmail.com>
2023-04-02 13:54:11 +0200

    fix: change default lang to DE

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="de">
       <Head />
       <body className="bg-white text-black">
         <Main />
